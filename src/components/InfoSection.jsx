a49bd616f12ad4c345f597e75cc78885c8676e59
Cristian Buitrago <Crisis1882@gmail.com>
2021-12-02 18:23:38 -0500

    Info section styles

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 const InfoSection = () => (
-  <div>
-    <p>
+  <div className="w-4/5 mx-auto">
+    <h3 className="text-red-500 font-semibold text-2xl mb-4">
+      What is a Fumo?
+    </h3>
+    <p className="w-full text-lg mb-4">
       Fumos are a line of high-quality plush character figures manufactured and
-      sold by the Japanese company <a href="https://www.gift-gift.jp/">Gift</a>. Fumos exist for multiple franchises,
-      but most notably the <a href="https://en.touhouwiki.net/wiki/Touhou_Wiki">Touhou Project</a>. Touhou Fumos are designed by
-      <a href="https://royalcat.xyz/">Royalcat</a>.
+      sold by the Japanese company <a href="https://www.gift-gift.jp/" className="text-red-500">Gift</a>.
+      Fumos exist for multiple franchises, but most notably the{" "}
+      <a href="https://en.touhouwiki.net/wiki/Touhou_Wiki" className="text-red-500">Touhou Project</a>.
+      Touhou Fumos are designed by{" "}
+      <a href="https://royalcat.xyz/" className="text-red-500">Royalcat</a>.
     </p>
-    <p>
+    <h3 className="text-red-500 font-semibold text-2xl mb-4">Accessibility:</h3>
+    <p className="w-full text-lg mb-4">
       Fumos are generally difficult to purchase, even in Japan, due to their
       relative scarcity. Fumos are out-of-stock as a rule, and Gift typically
       only manufactures them twice a year, when they open for limited
@@ -18,4 +24,4 @@ const InfoSection = () => (
   </div>
 );
 
-export default InfoSection
+export default InfoSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,14 @@ import HeroImage from "../components/HeroImage";
 import InfoSection from "../components/InfoSection";
 import FaqSection from "../components/FaqSection";
 
+// css
+import { waveBackground } from '../styles/index.module.css'
+
 const MainPage = () => {
   return (
     <Layout pageTitle="Home">
       <HeroImage />
+      <div className={`w-full aspect-w-16 aspect-h-6 bg-center bg-cover bg-no-repeat ${waveBackground}`}></div>
       <InfoSection />
       <FaqSection />
     </Layout>
