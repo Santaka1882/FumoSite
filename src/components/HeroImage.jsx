4b876d49b781a66144c0bb474b0fa7699d9427f4
Cristian Buitrago <Crisis1882@gmail.com>
2021-12-07 17:48:08 -0500

    Main page tablet styles

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
 const HeroImage = () => (
-  <div className="flex flex-col items-center w-full p-4 mb-4 mx-auto bg-red-500">
-    <StaticImage
-      src="../images/HeroImage.jpg"
-      alt="Reimu fumo"
-      className="w-60 max-w-sm rounded-full"
-    />
-    <div>
-      <h2 className="pt-4 text-yellow-200 font-semibold text-2xl text-center">Fumos</h2>
-      <p className="text-gray-100 text-lg text-center">
-        Everything you need to know about, more beautiful than ever.
-      </p>
+  <div className="flex flex-col items-center w-full py-8 my-4 bg-red-500">
+    <div className='flex flex-col items-center w-4/5 max-w-7xl mx-auto bg-red-500 md:flex-row-reverse justify-between space-x-5'>
+      <StaticImage
+        src="../images/HeroImage.jpg"
+        alt="Reimu fumo"
+        className="w-60 max-w-sm rounded-full md:rounded-lg"
+      />
+      <div className="max-w-sm text-center md:text-left">
+        <h2 className="pt-4 text-yellow-200 font-semibold text-2xl md:py-4 md:text-4xl">
+          Fumos
+        </h2>
+        <p className="max-w-sm text-gray-100 text-lg md:text-xl">
+          Everything you need to know about, more beautiful than ever.
+        </p>
+      </div>
     </div>
   </div>
 );
