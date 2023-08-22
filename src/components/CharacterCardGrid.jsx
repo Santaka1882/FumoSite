bc4a8746b16184dc6644cbbd636884d5924f1387
Cristian Buitrago <Crisis1882@gmail.com>
2023-08-22 15:44:20 -0500

    Added conditional rendering to the FumoList page and a nav to control what the page renders

--- src/components/CharacterCardGrid.jsx
+++ src/components/CharacterCardGrid.jsx
@@ -2,32 +2,69 @@ import React from 'react'
 
 import Card from './Card'
 
-const CharacterCardGrid = ({ title, regular, puppets, dekas, straps }) => {
+const CharacterCardGrid = ({ title, plushType, regular, puppets, dekas, straps }) => {
     return (
         <div className='w-full mx-auto mt-5 text-center md:text-left'>
-            <h3 className='text-xl text-red-500 font-semibold'>{title}</h3>
-            <div className='grid grid-cols-1 gap-2 mt-4 md:grid-cols-3 lg:grid-cols-4'>
+            <div>
                 {
-                    regular.map(plush => (
-                        <Card key={plush.name} name={plush.name} imageId={plush.id}/>
-                    ))
+                    plushType === "regulars" ?
+                    <>
+                        <h3 className='text-xl text-red-500 font-semibold'>{title}</h3>
+                        <div className='grid grid-cols-1 gap-2 mt-4 md:grid-cols-3 lg:grid-cols-4'>
+                            {
+                                regular.map(plush => (
+                                    <Card key={plush.name} name={plush.name} imageId={plush.id}/>
+                                ))
+                            }
+                            
+                        </div>
+                    </>   
+                    : null
                 }
-                {   puppets !== null &&
-                        puppets.map(plush => (
-                            <Card key={plush.name} name={plush.name} imageId={plush.id}/>
-                        ))
+                {   
+                    plushType === "puppets" ?
+                        puppets !== null &&
+                        <>
+                            <h3 className='text-xl text-red-500 font-semibold'>{title}</h3>
+                            <div className='grid grid-cols-1 gap-2 mt-4 md:grid-cols-3 lg:grid-cols-4'>
+                                {
+                                    puppets.map(plush => (
+                                        <Card key={plush.name} name={plush.name} imageId={plush.id}/>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    : null
+                }   
+                {
+                    plushType === "dekas" ?
+                        dekas !== null &&
+                        <>
+                            <h3 className='text-xl text-red-500 font-semibold'>{title}</h3>
+                            <div className='grid grid-cols-1 gap-2 mt-4 md:grid-cols-3 lg:grid-cols-4'>
+                                {
+                                    dekas.map(plush => (
+                                        <Card key={plush.name} name={plush.name} imageId={plush.id}/>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    : null
                 }
                 {
-                    dekas !== null &&
-                        dekas.map(plush => (
-                            <Card key={plush.name} name={plush.name} imageId={plush.id}/>
-                        ))
-                }
-                {
-                    straps !== null &&
-                        straps.map(plush => (
-                            <Card key={plush.name} name={plush.name} imageId={plush.id}/>
-                        ))
+                    plushType === "straps" ?
+                        straps !== null &&
+                        <>
+                            <h3 className='text-xl text-red-500 font-semibold'>{title}</h3>
+                            <div className='grid grid-cols-1 gap-2 mt-4 md:grid-cols-3 lg:grid-cols-4'>
+                                {
+                                    straps.map(plush => (
+                                        <Card key={plush.name} name={plush.name} imageId={plush.id}/>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    : null
                 }
             </div>           
         </div>
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useStaticQuery, graphql } from 'gatsby'
 import CharacterCardGrid from './CharacterCardGrid'
@@ -31,20 +31,56 @@ const Characters = () => {
     `)
 
     const characterList = data.allCharactersJson.nodes
-    
-    console.log(characterList)
+    const [plushType, setPlushType] = useState("regulars")
+
+    const regulars = () => setPlushType("regulars")
+    const dekas = () => setPlushType("dekas")
+    const straps = () => setPlushType("straps")
+    const puppets = () => setPlushType("puppets")
+
     return (
         <section className='w-full'>
             <div className='w-4/5 mx-auto mt-5 text-center'>
                 <h2 className='text-3xl text-red-500 font-semibold'>Fumo List</h2>
+
+                <nav className='w-full mt-4'>
+                    <ul className='grid grid-cols-2 md:grid-cols-4 gap-5 mx-auto'>
+                        <button 
+                            className='w-full bg-red-500 text-yellow-200 text-sm font-semibold tracking-wide rounded-md p-2 md:text-lg'
+                            onClick={regulars}
+                        >
+                            Regulars
+                        </button>
+                        <button 
+                            className='w-full bg-red-500 text-yellow-200 text-sm font-semibold tracking-wide rounded-md p-2 md:text-lg'
+                            onClick={dekas}
+                        >
+                            Dekas
+                        </button>
+                        <button 
+                            className='w-full bg-red-500 text-yellow-200 text-sm font-semibold tracking-wide rounded-md p-2 md:text-lg'
+                            onClick={straps}
+                        >
+                            Straps
+                        </button>
+                        <button 
+                            className='w-full bg-red-500 text-yellow-200 text-sm font-semibold tracking-wide rounded-md p-2 md:text-lg'
+                            onClick={puppets}
+                        >
+                            Puppets
+                        </button>
+                    </ul>
+                </nav>
+
                 {
                     characterList.map(character => (
                         <CharacterCardGrid
                             key={character.name}
                             title={character.name}
+                            plushType={plushType}
                             regular={character.regular}
                             puppets={character.puppets}
-                            dekas={character.dekas} 
+                            dekas={character.dekas}
                             straps={character.straps}
                         />
                     ))
